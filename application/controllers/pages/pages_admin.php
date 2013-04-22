12c95c02bbf9d22564739f1d609222b8c8837a18
Habib Pleines <habib@familiepleines.de>
2013-04-22 13:51:19 +0200

    test

--- application/controllers/pages/pages_admin.php
+++ application/controllers/pages/pages_admin.php
@@ -41,6 +41,7 @@ class Pages_Admin extends CI_Controller {
 		$this->load->view('backend/templates/admin/jquery-tablesorter-cp');
 		$this->load->view('backend/pages/pagesliste_admin', $data);
 		$this->load->view('backend/templates/admin/footer');	
+        
 	}
 }
 
