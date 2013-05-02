cf126a7329ab2508b3a2be0921968e9e7ad115a9
Habib Pleines <habib@familiepleines.de>
2013-05-02 11:41:42 +0200

    mittag

--- application/views/backend/templates/admin/tiny_mce_inc.php
+++ application/views/backend/templates/admin/tiny_mce_inc.php
@@ -1,46 +1,41 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed'); 
+if(!isset($config))  $config = 'default';
+
+$tinymce['default'] = '
+    script_url : "/js/tinymce/tinymce.min.js",
+    theme : "modern",
+    skin : "crazypixls",
+    language : "de",
+    width: "700",
+    height: "400",
+    doctype: "",
+    plugins : [ "advlist autolink autoresize charmap code contextmenu fullscreen hr link lists nonbreaking pagebreak paste preview spellchecker table visualblocks visualchars wordcount" ],
+    menu: "newdocument undo redo visualaid cut copy paste selectall bold italic underline strikethrough subscript superscript removeformat formats link charmap print preview hr pagebreak spellchecker searchreplace visualblocks visualchars code fullscreen insertdatetime nonbreaking inserttable tableprops deletetable cell row column",
+    pagebreak_separator: "<br>",
+    save_onsavecallback : "ajaxSave",
+    toolbar: "undo redo cut copy paste | link nonbreaking hr charmap | preview fullscreen | visualblocks visualchars code | spellchecker | styleselect | bold italic underline strikethrough subscript superscript | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent",     
+';
+
+$tinymce['pages'] = '
+    script_url : "/js/tinymce/tinymce.min.js",
+    theme : "modern",
+    skin : "crazypixls",
+    language : "de",
+    width: "700",
+    height: "400",
+    doctype: "",
+    plugins : [ "advlist autolink autoresize charmap code contextmenu fullscreen hr link lists nonbreaking pagebreak paste preview save spellchecker table visualblocks visualchars wordcount" ],
+    menu: "newdocument undo redo visualaid cut copy paste selectall bold italic underline strikethrough subscript superscript removeformat formats link charmap print preview hr pagebreak spellchecker searchreplace visualblocks visualchars code fullscreen insertdatetime nonbreaking inserttable tableprops deletetable cell row column",
+    pagebreak_separator: "<br>",
+    save_onsavecallback : "ajaxSave",
+    toolbar: "save | undo redo cut copy paste | link nonbreaking hr charmap | preview fullscreen | visualblocks visualchars code | spellchecker | styleselect | bold italic underline strikethrough subscript superscript | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent",     
+';
 
 ?>
 <script type="text/javascript">
-        $(function() {
-                $('textarea.tinymce').tinymce({
-                        script_url : '/js/tinymce/tinymce.min.js',
-                        theme : "modern",
-                        skin : 'crazypixls',
-                        language : 'de',
-                        width: 700,
-                        height: 400,
-                        doctype: '',
-                        plugins : [ 
-                            "advlist autolink autoresize autosave",
-                            "bbcode",
-                            "charmap code contextmenu",
-                            "directionality",
-                            "fullscreen",
-                            "hr",
-                            "image insertdatetime",
-                            "layer legacyoutput",
-                            "link lists",
-                            "nonbreaking noneditable",
-                            "pagebreak paste preview",
-                            "save searchreplace spellchecker",
-                            "tabfocus table template textcolor",
-                            "visualblocks visualchars wordcount" 
-                        ],
-                        menu: " ",
-                        save_onsavecallback : "ajaxSave",
-//                        style_formats: [
-//                                {title: 'Image Left', selector: 'img', styles: {
-//                                        'float' : 'left', 
-//                                        'margin': '0 10px 0 10px'
-//                                }},
-//                                {title: 'Image Right', selector: 'img', styles: {
-//                                        'float' : 'right', 
-//                                        'margin': '0 10px 0 10px'
-//                                }},
-//                        ],
-                        toolbar: "undo redo cut copy paste | styleselect | bold italic underline strikethrough subscript superscript | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | preview fullscreen | forecolor backcolor",    
-                        
-                });
-        });
+$(function() {
+    $('textarea.tinymce').tinymce({
+        <?=$tinymce[$config]?>                           
+    });
+});
 </script>
