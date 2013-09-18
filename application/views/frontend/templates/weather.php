f3aee4fef538741adc169864fe0fb57e36e0ecc6
Habib Pleines <habib@familiepleines.de>
2013-09-18 16:57:23 +0200

    Überarbeitung Startseite 1. Menüpunkt Read more bei Wetter entfernt 2. Grafik bei Wetter auskommentiert 3. Menüpunkt Read more bei Terminen mit korrektem Link versehen

--- application/views/frontend/templates/weather.php
+++ application/views/frontend/templates/weather.php
@@ -6,12 +6,12 @@
             <div class="weather">
             	<h1>Wetter Aussichten</h1>
                 <div class="wrapper">    
-                    <div class="icon"><?=$weather['weather_img']?></div>
+                   <!-- <div class="icon"><?=$weather['weather_img']?></div>-->
                     <div class="text">
                         <p class="grad"><?=$weather['weather_cond']['temp']?>&deg;<span class="celsius">Celsius</span></p>
                         <p class="status"><?=$weather['weather_cond']['text']?></p>
                     </div>
                 </div>
                 <hr class="clear" />
-                <p class="more"><a href="#" class="button_white">Read more</a></p>
+               <!-- <p class="more"><a href="#" class="button_white">Read more</a></p>-->
             </div>
