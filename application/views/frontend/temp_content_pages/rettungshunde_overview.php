7dd886636624f19d01678dca50bfb38b0b02aac2
Patrick <patrick.ritter@stud.tu-darmstadt.de>
2013-09-23 15:10:44 +0200

    Provisorische Rettungshundeeinheitsseite erstellt

--- application/views/frontend/temp_content_pages/rettungshunde_overview.php
+++ application/views/frontend/temp_content_pages/rettungshunde_overview.php
@@ -1,2 +1,49 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
 
+ <div id="MainContent">      
+            <div class="article">
+		         
+				<h1>Rettungshunde bei der Feuerwehr</h1>
+				<p>Da Rettungshundeeinheiten traditionell bei deutschen Feuerwehren nur vereinzelt vorhanden sind, möchten wir zunächst vorstellen, warum die Feuerwehr Bad Soden jetzt eine solche Einheit als Bestandteil ihrer Einsatzabteilung vorhält.</p>
+
+				<p>Nach dem HBKG (Hessisches Gesetz über den Brandschutz, die Allgemeine Hilfe und den Katastrophenschutz) haben hessische Feuerwehren unter anderem bei "Bränden, Explosionen, Unfällen oder andere Notlagen" Hilfe zu leisten. Im Falle von zerstörten Gebäudestrukturen ist für diese Hilfe der Einsatz von Hunden zum Aufspüren von Verschütteten unabdingbar (die biologische Ortungskomponente Hund kommt in diesem Fall dann zusammen mit technischen Komponenten zum Einsatz). Die Feuerwehr Bad Soden entschloss sich in Kooperation mit der Ortsgruppe Bad Soden des Vereins für Deutsche Schäferhunde, eine Rettungshunde-Ortungstechnikeinheit in Dienst zu stellen. Die Einheit ist offiziell seit Ende 2011 im Dienst.</p>
+
+				<p>Neben der Sucharbeit in Trümmergeländen werden die Hunde für Suchen in Flächengeländen (Wälder, Felder) ausgebildet. Alarmiert wird die Einheit durch die Rettungsleitstelle des Main-Taunus-Kreises nach Anforderung durch die örtliche Feuerwehr oder in Amtshilfe für die Polizei.</p>
+
+<h1>Ablauf der Ausbildung</h1>
+				<p>Die Ausbildung der Rettungshunde bei der Feuerwehr Bad Soden erfolgt gemäß den &laquo;Mindeststandards Rettungshunde-Ortungstechnik des Deutschen Feuerwehrverbandes&raquo; (siehe <a href="http://www.dfv.org/ak-rettungshunde.html/">www.dfv.org/ak-rettungshunde.html</a>). Der Hund sollte bei Ausbildungsbeginn nicht älter als 4 Jahre sein. Ohne Vorkenntnisse ist mit einer Ausbildungsdauer von grob 2 Jahren bis zum Erreichen der lokalen Einsatzfähigkeit (RH2) zu rechnen.</p>
+<p>Die Ausbildung erfolgt zu Beginn ausschließlich im Verein für Deutsche Schäferhunde in Bad Soden (<a href="http://www.svog-badsoden.de/">www.svog-badsoden.de</a>). Der angehende Rettungshund lernt spielerisch, Personen zu suchen, ungewohntes Terrain zu begehen, Hindernisse zu überwinden und bereitet sich zusammen mit seinem Hundeführer auf das Ablegen einer Begleithundeprüfung vor. In dieser zeigt der Hund den Grundgehorsam gegenüber seinem Hundeführer sowie die Unvoreingenommenheit gegenüber anderen Menschen und Hunden.</p>
+<p>Der Hundeführer tritt, falls er mit seinem Hund eine Einsatztätigkeit anstrebt, einer Feuerwehr bei und absolviert dort seine Feuerwehrgrundausbildung. Des Weiteren erhält er unter anderem eine Ausbildung in den Bereichen Suchtaktik, Funken, Erste Hilfe Hund und Mensch, Kynologie und Navigation.</p>
+<p>Sobald der Hund über Grundfertigkeiten in den Bereichen Gehorsam, Gewandtheit und Sucharbeit verfügt, demonstriert er diese in einer Rettungshundeprüfung RH1. Aus dieser folgt noch keine Einsatztauglichkeit, sie dient lediglich als Zwischenprüfung.</p>
+				<img src="http://rettungshundeeinheit.de/Ausbildung_neu.png" alt="Ausbildungablaufsschema"/>
+
+				<p>Mit Absolvieren einer Rettungshundeprüfung RH2 Trümmer oder Fläche, bei der Hund und Hundeführer die erfolgreiche Sucharbeit mit einem Truppmann unter Einsatzbedingungen zeigen, ist das Team aus Hund und Hundeführer für lokale und regionale Einsätze in der jeweiligen Sparte bereit und kommt bei Einsätzen im Main-Taunus-Kreis und angrenzenden Kreisen zum Einsatz. Die RH2 Prüfung muss jährlich wiederholt werden.</p>
+<p>Besonders geeignete Teams aus Hund und Hundeführer können sich darüber hinaus mit Absolvieren einer Rettungshundeprüfung RH3 Trümmer oder Fläche für internationale Einsätze qualifizieren. Mit einem Prüfungszeitraum von 24 Stunden (Fläche) bzw. 36 Stunden (Trümmer) fordern diese Prüfungen von Hund und Hundeführer weit über den Durchschnitt hinausgehende Leistungen. Die RH3 Prüfungen genügen den international anerkannten Standards der INSARAG (<a href="http://www.insarag.org">http://www.insarag.org</a>).</p>
+
+<h1>Interesse bekommen?</h1>
+				<p>Wir suchen engagierte Personen, die sich und ihren Hund zum Rettungshundeteam ausbilden lassen möchten. Wenn Sie neben der Motivation, regelmäßig Zeit in die Ausbildung ihres Hundes zu investieren, auch das Interesse an einer mittelfristigen Einsatztätigkeit in der Feuerwehr haben, würden wir Sie gerne bei einem unverbindlichen Schnuppertraining kennenlernen.</p>
+<p>Insbesondere besteht auch für aktive Feuerwehrangehörige anderer Feuerwehren die Möglichkeit, mit und ohne Hund bei der Einheit mitzuwirken.</p>
+
+				<h1>Trainingsgelände</h1>
+				<p>Außer neuen Rettungshundeteams sind wir auch immer an Übungsgeländen interessiert, um den Hunden ein möglichst abwechselungsreiches Training zu ermöglichen. Falls Sie über Abbruch-, Wald- oder Flächengelände verfügen, die Sie uns für ein Training zur Verfügung stellen möchten, würden wir uns über Ihre Kontaktaufnahme freuen.</p>
+
+            </div>
+        <hr class="clear" />
+        </div>    
+        <div id="SidebarContent" >   
+           	<div class="address">
+				<h1 class="first">Ansprechpartner</h1>
+				<p>Marco Bölling und Patrick Ritter</p>
+				<ul>
+					<li class="mail"><a href="mailto:mitmachen@rettungshundeeinheit.de">mitmachen@rettungshundeeinheit.de</a></li>
+				</ul>
+                <br />
+			</div>      
+		</div>   
+        <hr class="clear" />
+
+
+
+
+
+
