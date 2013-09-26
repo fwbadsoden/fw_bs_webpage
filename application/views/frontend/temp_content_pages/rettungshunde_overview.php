9696e64d7050f32613f045e96babe07c2cec6b21
Habib Pleines <habib@familiepleines.de>
2013-09-27 00:04:21 +0200

    RH Bilder

--- application/views/frontend/temp_content_pages/rettungshunde_overview.php
+++ application/views/frontend/temp_content_pages/rettungshunde_overview.php
@@ -1,49 +1,156 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
 
- <div id="MainContent">      
-            <div class="article">
-		         
-				<h1>Rettungshunde bei der Feuerwehr</h1>
-				<p>Da Rettungshundeeinheiten traditionell bei deutschen Feuerwehren nur vereinzelt vorhanden sind, möchten wir zunächst vorstellen, warum die Feuerwehr Bad Soden jetzt eine solche Einheit als Bestandteil ihrer Einsatzabteilung vorhält.</p>
+<div id="MainContent">  
+                
+                <div class="TabBox">
+                    <ul class="tabNav_details">
+                        <li><a href="#details_1" func="tab" class="active">Einleitung</a></li>
+                        <li><a href="#details_2" func="tab" class="noActive">Flächensuche</a></li>
+                        <li><a href="#details_3" func="tab" class="noActive">Trümmersuche</a></li>
+                    </ul>
+                    <div class="TabBoxContent">
+                        <h1 class="reiter"><a href="#details_1" func="tab" class="active">Einleitung</a></h1>
+                        <div id="box_details_1" style="">
+                            <h1>Rettungshunde bei der Feuerwehr</h1>
+                            <p>
+                                Rettungshunde sind zuverlässige Helfer bei der Rettung von in Not geratenen Menschen. Dank ihrem ausgeprägten Geruchssinn können sie große Gebiete effektiv und schnell nach einem Opfer absuchen.
+                                Ihre Hundenasen sind dabei so genau, dass es die gängige Technik nicht mit ihnen aufnehmen kann. Somit sind Rettungshunde mit ihrem Hundeführer im modernen Rettungswesen nicht mehr wegzudenken.
+                            </p>
+                            <p>
+                                Aus diesem Grund entschlossen wir uns 2011, gemeinsam mit der Ortsgruppe Bad Soden am Taunus im Verein für Deutsche Schäferhunde SV e.V., eine eigene "Facheinheit Rettungshunde-Ortungstechnik" 
+                                (RHOT) in Dienst zu stellen. Damit geht die Feuerwehr Bad Soden neue Wege, da Rettungshundeeinheiten bei deutschen Feuerwehren traditionell nur vereinzelt vorhanden sind.
+                            </p>
+                            <p><img src="<?=base_url('images/content_rettungshunde_einleitung.jpg')?>" /></p>
+                            <p>
+                                Unsere Rettungshundeteams sind, wie fast alle Mitglieder unserer Feuerwehr, Freiwillige. Gemeinsam mit ihren Hunden werden sie für die Trümmersuche und die Suche auf Flächengeländen, wie große 
+                                Waldflächen und anderen unwegsamen Geländen, ausgebildet. Auf diese Weise unterstützen sie die regulären Feuerwehreinheiten bei der Suche nach vermissten oder verletzten Personen. 
+                                Alarmiert wird die Einheit durch die Rettungsleitstelle des Main-Taunus-Kreises nach Anforderung durch die örtliche Feuerwehr oder in Amtshilfe für die Polizei.
+                            </p>
+                        </div>
+                        <h1 class="reiter"><a href="#details_2" func="tab" class="noActive">Flächensuche</a></h1>
+                        <div id="box_details_2" style="display: none;">
+                            <h1>Flächensuche</h1>
+                            <p>Bei einer Flächensuche sucht das Rettungshundeteam in großen Waldflächen oder auf anderem unwegsamen und dicht bewachsenem Gelände nach vermissten Personen. 
+                            Typischerweise handelt es sich bei den Einsätzen um Suchen nach vermissten Personen. Die Hunde werden dabei über das Gelände geführt, 
+                            so dass sie die menschliche Witterung aufnehmen können. Hat ein Hund eine Person gefunden, gibt es drei Anzeigemöglichkeiten, mit denen er seinem Hundeführer mitteilen kann, 
+                            wo sich die vermisste Person befindet.</p>
+                            <p><img src="<?=base_url('images/content_rettungshunde_flaechensuche.jpg')?>" /></p>
+                            <ul>
+                            	<li>Bellen: Der Hund bellt bei der gefundenen Person so lange, bis sein Hundeführer bei ihm ist</li>
+                                <li>Bringseln: Der Hund nimmt einen Gegenstand der Person auf und bringt ihn seinem Hundeführer. Anschließend führt er den Hundeführer zurück zu der vermissten Person.</li>
+                                <li>Freiverweisen: Der Hund führt seinen Hundeführer durch Pendeln zwischen Opfer und Hundeführer zur Fundstelle.</li>
+                            </ul>
+                        </div>
+                        <h1 class="reiter"><a href="#details_3" func="tab" class="noActive">Trümmersuche</a></h1>
+                        <div id="box_details_3" style="display: none;">
+                            <h1>Trümmersuche</h1>
+                            <p>Die Arbeit als Trümmersuchhund zählt zu den schwierigsten Formen der Rettungshundearbeit. Typische Einsätze sind Suchen nach vermissten Personen nach Gasexplosionen und Erdbeben. 
+                            Dabei müssen Hund und Hundeführer höchste Aufmerksamkeit und Konzentration walten lassen, da das Gelände oft unberechenbar und gefährlich ist. Solche Einsätze verlangen daher eine 
+                            hohe Disziplin von Hund und Mensch.</p>
+                            <p><img src="<?=base_url('images/content_rettungshunde_truemmersuche.jpg')?>" /></p>
+                            <p>Während der Suche muss der Rettungshund die menschliche Witterung durch meterdicken Schutt und Trümmerschichten aufnehmen. Zusätzlich sind bei solchen Katastrophen eine Vielzahl 
+                            von ablenkenden Gerüchen freigesetzt worden, so dass es für die Hunde nicht einfach ist, die richtige Witterung aufzunehmen. </p>
+							<p>Einsätze in ausländischen Katastrophengebieten erfordern eine außerordentlich hohe Disziplin und Belastbarkeit von Hund und Führer. Nur Rettungshundeteams mit der 
+                            Ausbildung RH3 kommen für solche Einsätze in Frage.</p>
+                        </div>
+                    </div>
+                </div>
+                
+ 	                     <div class="article">
+							<h1>Ablauf der Ausbildung</h1>
+                            <p>
+                                Die Ausbildung der Rettungshunde bei der Feuerwehr Bad Soden erfolgt gemäß den «Mindeststandards Rettungshunde-Ortungstechnik des Deutschen Feuerwehrverbandes» 
+                                (siehe <a href="http://www.dfv.org/ak-rettungshunde.html" target="_blank">http://www.dfv.org/ak-rettungshunde.html</a>). Der Hund sollte bei Ausbildungsbeginn nicht älter als 4 Jahre sein. Ohne Vorkenntnisse ist mit einer Ausbildungsdauer 
+                                von grob 2 Jahren bis zum Erreichen der lokalen Einsatzfähigkeit (RH2) zu rechnen.
+                            </p>
+                            <p>    
+                                Die Ausbildung erfolgt zu Beginn ausschließlich im Verein für Deutsche Schäferhunde in Bad Soden (<a href="http://www.svog-badsoden.de" target="_blank">http://www.svog-badsoden.de</a>). 
+                                Der angehende Rettungshund lernt spielerisch, Personen zu suchen, ungewohntes Terrain zu begehen, Hindernisse zu überwinden und bereitet sich zusammen mit seinem 
+                                Hundeführer auf das Ablegen einer Begleithundeprüfung vor. In dieser zeigt der Hund den Grundgehorsam gegenüber seinem Hundeführer, sowie die Unvoreingenommenheit 
+                                gegenüber anderen Menschen und Hunden.
+                            </p>
+                            <p>
+                                Der Hundeführer tritt, falls er mit seinem Hund eine Einsatztätigkeit anstrebt, einer Feuerwehr bei und absolviert dort seine Feuerwehrgrundausbildung. 
+                                Des Weiteren erhält er unter anderem eine Ausbildung in den Bereichen Suchtaktik, Funken, Erste Hilfe Hund und Mensch, Kynologie und Navigation.
+                            </p>
+                            <p>    
+                                Sobald der Hund über Grundfertigkeiten in den Bereichen Gehorsam, Gewandtheit und Sucharbeit verfügt, demonstriert er diese in einer Rettungshundeprüfung RH1. 
+                                Aus dieser folgt noch keine Einsatztauglichkeit, sie dient lediglich als Zwischenprüfung.
+                            </p>
+                            <p>
+                                Mit Absolvieren einer Rettungshundeprüfung RH2 Trümmer oder Fläche, bei der Hund und Hundeführer die erfolgreiche Sucharbeit mit einem Truppmann unter Einsatzbedingungen zeigen, 
+                                ist das Team aus Hund und Hundeführer für lokale und regionale Einsätze in der jeweiligen Sparte bereit und kommt bei Einsätzen im Main-Taunus-Kreis und angrenzenden Kreisen zum Einsatz. 
+                                Die RH2 Prüfung muss jährlich wiederholt werden.
+                            </p>
+                            <p>    
+                                Besonders geeignete Teams aus Hund und Hundeführer können sich darüber hinaus mit Absolvieren einer Rettungshundeprüfung RH3 Trümmer oder Fläche für internationale Einsätze qualifizieren. 
+                                Mit einem Prüfungszeitraum von 24 Stunden (Fläche) bzw. 36 Stunden (Trümmer) fordern diese Prüfungen von Hund und Hundeführer weit über den Durchschnitt hinausgehende Leistungen. 
+                                Die RH3 Prüfungen genügen den international anerkannten Standards der INSARAG (<a href="http://www.insarag.org" target="_blank">http://www.insarag.org</a>).                    
+                            </p>
+                        </div>
+            <div class="slideshow">
 
-				<p>Nach dem HBKG (Hessisches Gesetz über den Brandschutz, die Allgemeine Hilfe und den Katastrophenschutz) haben hessische Feuerwehren unter anderem bei "Bränden, Explosionen, Unfällen oder andere Notlagen" Hilfe zu leisten. Im Falle von zerstörten Gebäudestrukturen ist für diese Hilfe der Einsatz von Hunden zum Aufspüren von Verschütteten unabdingbar (die biologische Ortungskomponente Hund kommt in diesem Fall dann zusammen mit technischen Komponenten zum Einsatz). Die Feuerwehr Bad Soden entschloss sich in Kooperation mit der Ortsgruppe Bad Soden des Vereins für Deutsche Schäferhunde, eine Rettungshunde-Ortungstechnikeinheit in Dienst zu stellen. Die Einheit ist offiziell seit Ende 2011 im Dienst.</p>
-
-				<p>Neben der Sucharbeit in Trümmergeländen werden die Hunde für Suchen in Flächengeländen (Wälder, Felder) ausgebildet. Alarmiert wird die Einheit durch die Rettungsleitstelle des Main-Taunus-Kreises nach Anforderung durch die örtliche Feuerwehr oder in Amtshilfe für die Polizei.</p>
-
-<h1>Ablauf der Ausbildung</h1>
-				<p>Die Ausbildung der Rettungshunde bei der Feuerwehr Bad Soden erfolgt gemäß den &laquo;Mindeststandards Rettungshunde-Ortungstechnik des Deutschen Feuerwehrverbandes&raquo; (siehe <a href="http://www.dfv.org/ak-rettungshunde.html/">www.dfv.org/ak-rettungshunde.html</a>). Der Hund sollte bei Ausbildungsbeginn nicht älter als 4 Jahre sein. Ohne Vorkenntnisse ist mit einer Ausbildungsdauer von grob 2 Jahren bis zum Erreichen der lokalen Einsatzfähigkeit (RH2) zu rechnen.</p>
-<p>Die Ausbildung erfolgt zu Beginn ausschließlich im Verein für Deutsche Schäferhunde in Bad Soden (<a href="http://www.svog-badsoden.de/">www.svog-badsoden.de</a>). Der angehende Rettungshund lernt spielerisch, Personen zu suchen, ungewohntes Terrain zu begehen, Hindernisse zu überwinden und bereitet sich zusammen mit seinem Hundeführer auf das Ablegen einer Begleithundeprüfung vor. In dieser zeigt der Hund den Grundgehorsam gegenüber seinem Hundeführer sowie die Unvoreingenommenheit gegenüber anderen Menschen und Hunden.</p>
-<p>Der Hundeführer tritt, falls er mit seinem Hund eine Einsatztätigkeit anstrebt, einer Feuerwehr bei und absolviert dort seine Feuerwehrgrundausbildung. Des Weiteren erhält er unter anderem eine Ausbildung in den Bereichen Suchtaktik, Funken, Erste Hilfe Hund und Mensch, Kynologie und Navigation.</p>
-<p>Sobald der Hund über Grundfertigkeiten in den Bereichen Gehorsam, Gewandtheit und Sucharbeit verfügt, demonstriert er diese in einer Rettungshundeprüfung RH1. Aus dieser folgt noch keine Einsatztauglichkeit, sie dient lediglich als Zwischenprüfung.</p>
-				<img src="http://rettungshundeeinheit.de/Ausbildung_neu.png" alt="Ausbildungablaufsschema"/>
-
-				<p>Mit Absolvieren einer Rettungshundeprüfung RH2 Trümmer oder Fläche, bei der Hund und Hundeführer die erfolgreiche Sucharbeit mit einem Truppmann unter Einsatzbedingungen zeigen, ist das Team aus Hund und Hundeführer für lokale und regionale Einsätze in der jeweiligen Sparte bereit und kommt bei Einsätzen im Main-Taunus-Kreis und angrenzenden Kreisen zum Einsatz. Die RH2 Prüfung muss jährlich wiederholt werden.</p>
-<p>Besonders geeignete Teams aus Hund und Hundeführer können sich darüber hinaus mit Absolvieren einer Rettungshundeprüfung RH3 Trümmer oder Fläche für internationale Einsätze qualifizieren. Mit einem Prüfungszeitraum von 24 Stunden (Fläche) bzw. 36 Stunden (Trümmer) fordern diese Prüfungen von Hund und Hundeführer weit über den Durchschnitt hinausgehende Leistungen. Die RH3 Prüfungen genügen den international anerkannten Standards der INSARAG (<a href="http://www.insarag.org">http://www.insarag.org</a>).</p>
-
-<h1>Interesse bekommen?</h1>
-				<p>Wir suchen engagierte Personen, die sich und ihren Hund zum Rettungshundeteam ausbilden lassen möchten. Wenn Sie neben der Motivation, regelmäßig Zeit in die Ausbildung ihres Hundes zu investieren, auch das Interesse an einer mittelfristigen Einsatztätigkeit in der Feuerwehr haben, würden wir Sie gerne bei einem unverbindlichen Schnuppertraining kennenlernen.</p>
-<p>Insbesondere besteht auch für aktive Feuerwehrangehörige anderer Feuerwehren die Möglichkeit, mit und ohne Hund bei der Einheit mitzuwirken.</p>
-
-				<h1>Trainingsgelände</h1>
-				<p>Außer neuen Rettungshundeteams sind wir auch immer an Übungsgeländen interessiert, um den Hunden ein möglichst abwechselungsreiches Training zu ermöglichen. Falls Sie über Abbruch-, Wald- oder Flächengelände verfügen, die Sie uns für ein Training zur Verfügung stellen möchten, würden wir uns über Ihre Kontaktaufnahme freuen.</p>
-
+                <div class="prevPic"><a href="#slideshow_car" id="slideshow_prev"><img src="<?=base_url('images/layout/button_detailShow_previous.png')?>" /></a></div>
+                <div class="nextPic"><a href="#slideshow_car" id="slideshow_next"><img src="<?=base_url('images/layout/button_detailShow_next.png')?>" /></a></div>
+                
+                <ul id="slideshow_car">
+                    <li id="slideshow_car_1" class="active">
+                        <figure>
+                        	<img src="<?=base_url('images/content/anhaenger.jpg')?>" alt="" />
+                        	<div class="zoom"><a href="<?=base_url('images/content/anhaenger.jpg')?>" class="fancybox-gallery" rel="gallery1"><img src="<?=base_url('images/layout/button_zoom.png')?>" /></a></div>
+                        </figure>
+                    </li>
+                    <li id="slideshow_car_2" >
+                        <figure>
+                        	<img src="<?=base_url('images/content/hund_black.jpg')?>" alt="" />
+                        	<div class="zoom"><a href="<?=base_url('images/content/hund_black.jpg')?>" class="fancybox-gallery" rel="gallery1"><img src="<?=base_url('images/layout/button_zoom.png')?>" /></a></div>
+                        </figure>
+                    </li>
+                    <li id="slideshow_car_3" >
+                        <figure>
+                        	<img src="<?=base_url('images/content/uebung_2.jpg')?>" alt="" />
+                            <div class="zoom"><a href="<?=base_url('images/content/uebung_2.jpg')?>" class="fancybox-gallery" rel="gallery1"><img src="<?=base_url('images/layout/button_zoom.png')?>" /></a></div>
+                        </figure>
+                    </li>
+                    <li id="slideshow_car_4" >
+                        <figure>
+                        	<img src="<?=base_url('images/content/uebung_1.jpg')?>" alt="" />
+                        	<div class="zoom"><a href="<?=base_url('images/content/uebung_1.jpg')?>" class="fancybox-gallery" rel="gallery1"><img src="<?=base_url('images/layout/button_zoom.png')?>" /></a></div>
+                        </figure>
+                    </li>
+                    <li id="slideshow_car_5">
+                        <figure>
+                        	<img src="<?=base_url('images/content/rettungshundeteam.jpg')?>" alt="" />
+                            <div class="zoom"><a href="<?=base_url('images/content/rettungshundeteam.jpg')?>" class="fancybox-gallery" rel="gallery1" title="loremipsum"><img src="<?=base_url('images/layout/button_zoom.png')?>" /></a></div>
+                        </figure>
+                    </li>
+                </ul>
             </div>
-        <hr class="clear" />
-        </div>    
-        <div id="SidebarContent" >   
+
+
+        </div>
+        <div id="SidebarContent">   
            	<div class="address">
-				<h1 class="first">Ansprechpartner</h1>
-				<p>Marco Bölling und Patrick Ritter</p>
+				<h1 class="first">Kontaktadresse</h1>
+				<p>Rettungshundeeinheit<br/>der Freiwilligen Feuerwehr<br/>Bad Soden am Taunus</p>
+				<p>Marco Bölling<br/>Patrick Ritter</p>
 				<ul>
 					<li class="mail"><a href="mailto:mitmachen@rettungshundeeinheit.de">mitmachen@rettungshundeeinheit.de</a></li>
 				</ul>
-                <br />
-			</div>      
-		</div>   
+			</div>    
+			<div class="textTeaser">    
+				<h1>Interesse bekommen?</h1>
+				<p>Wir suchen engagierte Personen, die sich und ihren Hund zum Rettungshundeteam ausbilden lassen möchten. </p>
+                <p>
+                    Wenn Sie neben Motivation, regelmäßig Zeit für die Ausbildung ihres Hundes und Interesse am Mitwirken in der Feuerwehr haben, 
+                    schauen Sie bei unserem unverbindlichen Schnuppertraining vorbei.
+                </p>
+				<p>Insbesondere aktive Feuerwehrangehörige anderer Feuerwehren können mit und ohne Hund bei der Einheit mitwirken.</h2>
+			</div>
+			<div class="textTeaser">    
+				<h1>Trainingsgelände</h1>
+				<p>Sie können uns auch ohne einen Hund zu besitzen unterstützen. Um den Hunden ein möglichst abwechselungsreiches Training zu ermöglichen suchen wir immer geeignete Trainingsgelände. 
+                Wenn Sie über Abbruch-, Wald- oder Flächengelände verfügen, die Sie uns für ein Training zur Verfügung stellen möchten, würden wir uns sehr freuen.</p>
+			</div>
+		</div>
         <hr class="clear" />
-
-
-
-
-
-
