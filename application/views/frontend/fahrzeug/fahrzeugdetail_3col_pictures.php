4a5851a044fa136715b3e6ba721cf5bf31b2fecf
Patrick <pa_ritter@arcor.de>
2013-09-30 17:15:34 +0200

    Fahrzeuggallerie war bei nur einem Fahrzeugfoto fehlerhaft
    Wenn nur ein Foto in der Fahrzeuggallerie war, konnte man aus der
    Gallerie rausnavigieren.
    
    Außerdem den Fix für "Nächstes Fahrzeuggalleriebild kommt beim
    ersten Klick" gefixt.

--- application/views/frontend/fahrzeug/fahrzeugdetail_3col_pictures.php
+++ application/views/frontend/fahrzeug/fahrzeugdetail_3col_pictures.php
@@ -5,15 +5,17 @@
         <div class="BigBox firstColumn">  
             <div class="slideshow">
 
+		<?php if(count($images) > 1) {?>
                 <div class="prevPic"><a href="#slideshow_car" id="slideshow_prev"><img src="<?=base_url('images/layout/button_detailShow_previous.png')?>" /></a></div>
                 <div class="nextPic"><a href="#slideshow_car" id="slideshow_next"><img src="<?=base_url('images/layout/button_detailShow_next.png')?>" /></a></div>
-                
+		<?php }?>                
+
                 <ul id="slideshow_car">
-<? foreach($images as $key => $img) : ?>                
-<? if($key == 1) : ?>
-                    <li id="slideshow_car_<?=$key?>" class="active">
+<? foreach($images as $key => $img) : ?>           
+<? if($key == 0) : ?>
+                    <li id="slideshow_car_<?=$key+1?>" class="active">
 <? else : ?>
-                    <li id="slideshow_car_<?=$key?>" class="noActive">
+                    <li id="slideshow_car_<?=$key+1?>" class="noActive">
 <? endif; ?>
                         <figure>
                         	<img src="<?=base_url(CONTENT_IMG_FAHRZEUG_UPLOAD_PATH.$img['img_file'])?>" alt="<?=$img['description']?>" />
