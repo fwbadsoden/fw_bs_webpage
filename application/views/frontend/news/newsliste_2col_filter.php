78ec8c3f7be1b6e27e0288d042b59ccafa730751
Habib Pleines <habib@familiepleines.de>
2013-10-24 12:39:32 +0200

    Viewfiles für Übersichtsseite

---
+++ application/views/frontend/news/newsliste_2col_filter.php
@@ -0,0 +1,47 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed'); 
+    $this->load->helper('html'); 
+	$this->load->helper('form');    
+    
+    $form = array(
+        'id'   => 'einsatzFilter',
+        'name' => 'einsatzFilter'
+    );
+	
+	$year_options = array();
+	if(isset($_POST['einsatzJahr'])) $year_selected = $_POST['einsatzJahr']; 
+	else 							 $year_selected = 0;
+	$year_attr = "class = 'input_dropdown' id = 'input_dropdown' onChange='this.form.submit()'";
+	foreach($years as $year)
+	{
+		$year_options[$year] = $year;	
+	}
+	
+	$type_options = array();
+	if(isset($_POST['einsatzArt'])) $type_selected = $_POST['einsatzArt']; 
+	else 							$type_selected = 0;
+	$type_attr = 'class = "input_dropdown" id = "input_dropdown" onChange="this.form.submit()"';
+    $type_options[0] = 'Alle Einsätze';
+	foreach($types as $type)
+	{
+		$type_options[$type['typeID']] = $type['typeName'];	
+	}    
+?> 
+
+        <div class="oneColumnBox" id="submenue">
+        
+        	<div class="filter">
+            	<div class="filterBox" id="einsatzJahr">
+                <?=form_open(current_url(), $form);?>
+                    <div class="styled-select">
+                        <?=form_dropdown('einsatzJahr', $year_options, $year_selected, $year_attr)?>
+                	</div>
+                    <div class="styled-select">
+                        <?=form_dropdown('einsatzArt', $type_options, $type_selected, $type_attr)?>
+                	</div>
+                    <div><a href="#top" class="backToTop" rel="nicescrolling"></a></div>
+	                <hr class="clear" />
+                </div>
+            </div>
+        
+        </div>
+        <div class="jsplatzhalter"></div>
---
+++ application/views/frontend/news/newsliste_2col_header.php
@@ -0,0 +1,5 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed'); 
+    $this->load->helper('html'); 
+?> 
+
+        <div class="oneColumnBox">    
