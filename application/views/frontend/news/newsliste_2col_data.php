9b77158541ef0901ac2c78acfe5102c4f6518317
Habib Pleines <habib@familiepleines.de>
2013-10-24 23:17:22 +0200

    Newsübersicht

--- application/views/frontend/news/newsliste_2col_data.php
+++ application/views/frontend/news/newsliste_2col_data.php
@@ -4,13 +4,12 @@
             <div class="listContent">
 
 				<div class="row">
-                	<div class="number trenner<?=$special_class?>"><a href="<?=base_url('aktuelles/einsatz/'.$id)?>"><?=$einsatz_nr?></a></div>
-                	<div class="date trenner<?=$special_class?>"><a href="<?=base_url('aktuelles/einsatz/'.$id)?>"><span class="inline_date"><?=cp_get_ger_date($datum_beginn)?></span> <span class="inline_slash">/</span> <span class="inline_time"><?=$uhrzeit_beginn?> Uhr</span></a></div>
-                	<div class="icon trenner<?=$special_class?>"><a href="<?=base_url('aktuelles/einsatz/'.$id)?>" class="<?=$type_class?>"><?=$type_class?></a></div>
-                	<div class="headline<?=$special_class?>"><a href="<?=base_url('aktuelles/einsatz/'.$id)?>"><?=$name?></a></div>
-                    <div class="moreButton<?=$special_class?>"><a href="<?=base_url('aktuelles/einsatz/'.$id)?>" class="button_black">Mehr lesen</a></div>
-                    <div class="moreButton_mobile<?=$special_class?>"><a href="<?=base_url('aktuelles/einsatz/'.$id)?>" class="button_blackarrow_mobile">&nbsp;</a></div>
+                	<!--<div class="number trenner"><a href="<?=base_url('aktuelles/news/'.$id)?>"><?=$id?></a></div>-->
+                	<div class="date trenner"><a href="<?=base_url('aktuelles/news/'.$id)?>"><span class="inline_date"><?=cp_get_ger_date($valid_from)?></span></a></div>
+                <!--	<div class="icon trenner"><a href="<?=base_url('aktuelles/news/'.$id)?>" class="<?=$type_class?>"><?=$type_class?></a></div>-->
+                	<div class="headline"><a href="<?=base_url('aktuelles/news/'.$id)?>"><?=$title?></a><br /><?=$teaser?></div>
+                    <div class="moreButton"><a href="<?=base_url('aktuelles/news/'.$id)?>" class="button_black">Mehr lesen</a></div>
+                    <div class="moreButton_mobile"><a href="<?=base_url('aktuelles/news/'.$id)?>" class="button_blackarrow_mobile">&nbsp;</a></div>
                 </div>
-                
             </div>
 			<hr class="clear" />
--- application/views/frontend/news/newsliste_2col_filter.php
+++ application/views/frontend/news/newsliste_2col_filter.php
@@ -3,40 +3,28 @@
 	$this->load->helper('form');    
     
     $form = array(
-        'id'   => 'einsatzFilter',
-        'name' => 'einsatzFilter'
+        'id'   => 'newsFilter',
+        'name' => 'newsFilter'
     );
 	
-	$year_options = array();
-	if(isset($_POST['einsatzJahr'])) $year_selected = $_POST['einsatzJahr']; 
-	else 							 $year_selected = 0;
-	$year_attr = "class = 'input_dropdown' id = 'input_dropdown' onChange='this.form.submit()'";
-	foreach($years as $year)
+	$category_options = array();
+	if(isset($_POST['newsArt']))    $category_selected = $_POST['newsArt']; 
+	else 							$category_selected = 0;
+	$category_attr = 'class = "input_dropdown" id = "input_dropdown" onChange="this.form.submit()"';
+    $category_options[0] = 'Alle News';
+	foreach($categories as $category)
 	{
-		$year_options[$year] = $year;	
-	}
-	
-	$type_options = array();
-	if(isset($_POST['einsatzArt'])) $type_selected = $_POST['einsatzArt']; 
-	else 							$type_selected = 0;
-	$type_attr = 'class = "input_dropdown" id = "input_dropdown" onChange="this.form.submit()"';
-    $type_options[0] = 'Alle Einsätze';
-	foreach($types as $type)
-	{
-		$type_options[$type['typeID']] = $type['typeName'];	
+		$category_options[$category['categoryID']] = $category['title'];	
 	}    
 ?> 
 
         <div class="oneColumnBox" id="submenue">
         
         	<div class="filter">
-            	<div class="filterBox" id="einsatzJahr">
+            	<div class="filterBox" id="newsJahr">
                 <?=form_open(current_url(), $form);?>
                     <div class="styled-select">
-                        <?=form_dropdown('einsatzJahr', $year_options, $year_selected, $year_attr)?>
-                	</div>
-                    <div class="styled-select">
-                        <?=form_dropdown('einsatzArt', $type_options, $type_selected, $type_attr)?>
+                        <?=form_dropdown('newsArt', $category_options, $category_selected, $category_attr)?>
                 	</div>
                     <div><a href="#top" class="backToTop" rel="nicescrolling"></a></div>
 	                <hr class="clear" />
