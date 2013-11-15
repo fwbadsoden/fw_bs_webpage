9b7ba19d483fac0dc53246370a14679377988c40
Habib Pleines <habib@familiepleines.de>
2013-11-15 13:22:03 +0100

    autosuggest.php

--- application/views/backend/einsatz/autosuggest.php
+++ application/views/backend/einsatz/autosuggest.php
@@ -12,6 +12,34 @@ $(function() {
       position: { my : "left top", at: "right top" },
       autofocus: true,
     });
+
+//	  function split( val ) {
+//    return val.split( /,\s*/ );
+//  }
+//
+//  function extractLast( term ) {
+//     return split( term ).pop();
+//   }
+//
+//   $( "#weitereeinsatzkraefte" )
+//        .autocomplete({
+//             minLength: 0,
+//             source: function( request, response ) {
+//                 response( $.ui.autocomplete.filter(
+//                     items, extractLast( request.term ) ) );
+//             },
+//             focus: function() {
+//                 return false;
+//             },
+//            select: function( event, ui ) {
+//                var terms = split( this.value );
+//                terms.pop();
+//                terms.push( ui.item.value );
+//                terms.push( "" );
+//                this.value = terms.join( ", " );
+//                return false;
+//            }
+//        });
 });
         
 </script>
