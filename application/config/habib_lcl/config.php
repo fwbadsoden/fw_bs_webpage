359fdb90d853bf83c7e3c55feffb756c80871f90
Habib Pleines <habib@familiepleines.de>
2014-05-04 10:18:17 +0200

    local config

--- application/config/habib_lcl/config.php
+++ application/config/habib_lcl/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/feuerwehr-bs.de';
+$config['base_url']	= 'http://fw_bs_webpage/';
 /*
 |--------------------------------------------------------------------------
 | Index File
